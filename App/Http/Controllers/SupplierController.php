ee1b2d2e1bb21cdbfe46b1ee28d620432c101622
Maged Groups <magedyaseengroups@gmail.com>
2026-02-17 21:22:13 +0200

    feat: add props to SupplierController

--- App/Http/Controllers/SupplierController.php
+++ App/Http/Controllers/SupplierController.php
@@ -4,5 +4,7 @@ namespace App\Http\Controllers;
 
 class SupplierController extends Controller
 {
-    // 
+    public static string $name;
+    public static string $address;
+    public static string $city;
 }

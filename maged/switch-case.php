8d07a773baea40c1896bdd184b4e2041ca067925
Aly Mohamed <aly.mohamed2707@gmail.com>
2026-03-08 11:38:04 +0200

    spaces

--- maged/switch-case.php
+++ maged/switch-case.php
@@ -9,7 +9,11 @@ ControlStructureController::switchCase(dayNumber: 3);
 $enDayName= 'Wednesday';
 $arDayName = ControlStructureController::getDayArName($enDayName);
 echo "<h2>The Aabic day name for $enDayName is $arDayName</h2>";
-
+echo"<hr>";
+echo"<hr>";
 
 $day = 'Friday';
 $message = ControlStructureController::weekDaysMessage($message);
+echo "<h2>Hey since it is $day so, $message</h2>";
+echo"<hr>";
+echo"<hr>";
